bdf1612926af85a475bb32af247a94655cd6de17
alejandromartincruz <socorreitor@gmail.com>
2018-09-29 17:50:57 +0200

    configure sass, basic styling and add routing module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { 
+    Router, NavigationStart, NavigationCancel, NavigationEnd 
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'classpip-onboarding';
+  loading;
+
+  constructor(
+    private router: Router
+  ) {
+    this.loading = true;
+  }
+
+  ngAfterViewInit() {
+    this.router.events
+      .subscribe((event) => {
+        if(event instanceof NavigationStart) {
+          this.loading = true;
+        }
+        else if (
+          event instanceof NavigationEnd || 
+          event instanceof NavigationCancel
+        ) {
+          this.loading = false;
+        }
+      });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LoadingComponent } from './shared/loading/loading.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoadingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
